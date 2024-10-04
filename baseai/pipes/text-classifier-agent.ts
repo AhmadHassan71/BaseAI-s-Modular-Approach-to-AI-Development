41d8538d0bdc0566094e3a37a9d0cc45786d3f6d
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2024-10-04 23:55:15 +0500

    fix the code to run according to base AI docs

--- baseai/pipes/text-classifier-agent.ts
+++ baseai/pipes/text-classifier-agent.ts
@@ -1,4 +1,4 @@
-// baseai/pipes/text-classifier.pipe.ts
+// baseai/pipes/text-classifier-agent.ts
 import { PipeI } from '@baseai/core';
 
 const textClassifierPipe = (): PipeI => ({
@@ -7,11 +7,23 @@ const textClassifierPipe = (): PipeI => ({
   description: 'Pipe to classify text as positive, negative, or neutral',
   status: 'private',
   model: 'openai:gpt-4o-mini',
-  task: async (input: string) => {
-    const categories = ['positive', 'negative', 'neutral'];
-    const category = categories[Math.floor(Math.random() * categories.length)];
-    return `Classified as: ${category}`;
-  },
+  stream: true,
+  json: false,
+  store: true,
+  moderate: true,
+  top_p: 1,
+  max_tokens: 1000,
+  temperature: 0.7,
+  presence_penalty: 1,
+  frequency_penalty: 1,
+  stop: [],
+  tool_choice: 'auto',
+  parallel_tool_calls: false,
+  messages: [{ role: 'system', content: `You are an expert at classifying sentences
+  as positive, negative or neutral. You will classify the sentences into those types` }],
+  variables: [],
+  memory: [],
+  tools: []
 });
 
 export default textClassifierPipe;
--- index.ts
+++ index.ts
@@ -1,16 +1,35 @@
 // index.ts
-import combinedPipe from './baseai/pipes/combined-agent.ts';
+import 'dotenv/config';
+import textClassifierPipe from './baseai/pipes/text-classifier-agent.ts';
+import { Pipe, getRunner } from '@baseai/core';
+
+const pipe = new Pipe(textClassifierPipe());
+const message = 'Ali is a good person';
 
 async function run() {
-  const url = process.env.URL; 
-  const pipe = combinedPipe();
+  const { stream } = await pipe.run({
+  messages: [{ role: 'user', content: message}],
+  stream: true
+ });
 
-  try {
-    const result = await pipe.run(url!!);
-    console.log('Classification Result:', result);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+ const runner = getRunner(stream);
+
+ 
+ runner.on('connect', () => {
+  console.log('Stream started.\n');
+ });
+
+ runner.on('content', content => {
+  process.stdout.write(content);
+ });
+
+ runner.on('end', () => {
+  console.log('\nStream ended.');
+ });
+
+ runner.on('error', error => {
+  console.error('Error:', error);
+ });
 }
 
 run();
